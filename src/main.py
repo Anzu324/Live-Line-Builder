948936a0cb7e0f9d9df9fe65af0ae8368eb95f46
Anzu324 <oz_sk_as_2002@outlook.jp>
2026-08-17 12:11:25 +0900

    PyQtDarkThemeを導入

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 
+import qdarktheme
 from PySide6.QtWidgets import QApplication
 
 from ui.views.main_window import MainWindow
@@ -7,8 +8,11 @@ from ui.views.main_window import MainWindow
 # 1. アプリケーションの作成（必須）
 app = QApplication(sys.argv)
 
+qdarktheme.setup_theme()
 # 2. ウィジェット（画面パーツ）の作成と設定
 main_window = MainWindow()
 main_window.show()
 
 sys.exit(app.exec())
+
+app.exec()
