4d033c740acc26a9908e51bf76fb3088e760b665
Anzu324 <oz_sk_as_2002@outlook.jp>
2026-08-18 13:08:41 +0900

    スクロールバーが表示される問題を修正

--- src/live_line_builder/ui/views/table.py
+++ src/live_line_builder/ui/views/table.py
@@ -12,25 +12,25 @@ class EquipmentTableView(QWidget):
 
         self._model = source_model
 
-        self.view = QTableView(self)  # QTableViewのインスタンスを作成
-        self.view.setModel(source_model)
-        self.view.resize(600, 500)
-        self.view.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
-        self.view.verticalHeader().setVisible(False)
-        self.adjust_table_height()
+        self.table = QTableView(self)  # QTableViewのインスタンスを作成
+        self.table.setModel(source_model)
+        self.table.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
+        self.table.verticalHeader().setVisible(False)
 
-        self.view.horizontalHeader().setSectionResizeMode(
+        # サイズ調整
+        self.table.horizontalHeader().setSectionResizeMode(
             QHeaderView.ResizeMode.ResizeToContents
         )  # 列の幅を内容に合わせて自動調整する
-        self.view.horizontalHeader().setSectionResizeMode(
+        self.table.horizontalHeader().setSectionResizeMode(
             0, QHeaderView.ResizeMode.Stretch
         )  # 名前列に限り幅を最大になるよう調整する
-        self.view.setColumnWidth(1, 50)  # タイプ列の幅を50pxに固定する
+        self.table.setColumnWidth(1, 50)  # タイプ列の幅を50pxに固定する
+        self.adjust_table_height()  # 全体の高さの最小値をスクロール値されないように設定
 
         # レイアウトの作成とテーブルの追加
         self.layout_a = QVBoxLayout()
         self.layout_a.addWidget(title_label)
-        self.layout_a.addWidget(self.view)
+        self.layout_a.addWidget(self.table)
 
         self.layout_a.setContentsMargins(0, 0, 0, 0)
         self.layout_a.setSpacing(1)
@@ -38,14 +38,14 @@ class EquipmentTableView(QWidget):
 
     def adjust_table_height(self):
         h = (
-            self.view.horizontalHeader().height()
-            + self.view.verticalHeader().length()
-            + (self.view.frameWidth() * 2)
+            self.table.horizontalHeader().height()
+            + self.table.verticalHeader().length()
+            + (self.table.frameWidth() * 2)
         )
 
         # スクロールバーが出ない最小サイズとして設定
-        self.view.setMinimumHeight(h)
+        self.table.setMinimumHeight(h)
 
     def set_model_of_table(self, source_model: QAbstractItemModel):
         self._model = source_model
-        self.view.setModel(source_model)
+        self.table.setModel(source_model)
