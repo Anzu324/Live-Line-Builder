4ee480ea11d59d494b296e9f50656e357c3cc4df
Anzu324 <oz_sk_as_2002@outlook.jp>
2026-08-19 15:11:50 +0900

    test作成の準備を行った

--- src/live_line_builder/__init__.py
+++ src/live_line_builder/__init__.py
@@ -6,16 +6,22 @@ from PySide6.QtWidgets import QApplication
 from live_line_builder.ui.app_controller import AppController
 from live_line_builder.ui.views.main_window import MainWindow
 
-# 1. アプリケーションの作成（必須）
-app = QApplication(sys.argv)
 
-# テーマをセット
-qdarktheme.setup_theme()
+def main():
+    # 1. アプリケーションの作成（必須）
+    app = QApplication(sys.argv)
 
-# アプリの管理システムを起動
-app_controller = AppController.factory_by_mock()
-# 2. ウィジェット（画面パーツ）の作成と設定
+    # テーマをセット
+    qdarktheme.setup_theme()
 
-sys.exit(app.exec())
+    # アプリの管理システムを起動
+    _app_controller = AppController.factory_by_mock()
+    # 2. ウィジェット（画面パーツ）の作成と設定
 
-app.exec()
+    sys.exit(app.exec())
+
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/graph/test_graph_generate.py
@@ -0,0 +1,5 @@
+from live_line_builder.domain.line_graph.audio_patch import AudioPatchSystem
+
+
+def test_audio_patch_system_generate():
+    AudioPatchSystem()
