a1729c31b771eb2c284d8a53cc2782de0c9ce421
Anzu324 <oz_sk_as_2002@outlook.jp>
2026-08-30 09:38:04 +0900

    Add crew info to LiveInfoView.

--- src/live_line_builder/ui/views/live_info_view.py
+++ src/live_line_builder/ui/views/live_info_view.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QFormLayout, QLineEdit, QWidget
+from PySide6.QtWidgets import QFormLayout, QGridLayout, QLineEdit, QWidget
 
 
 # メインコンテンツビューのクラス
@@ -11,14 +11,30 @@ class LiveInfoView(QWidget):
         super().__init__(parent)
 
         # ライブ情報エリア
-        self.formLayout = QFormLayout()
+        self.grid_layout = QGridLayout()
+        self.show_form_Layout = QFormLayout()
         self.show_name = QLineEdit()
         self.show_place = QLineEdit()
         self.show_day = QLineEdit()
-        self.formLayout.addRow(self.tr("公演名"), self.show_name)
-        self.formLayout.addRow(self.tr("開場"), self.show_place)
-        self.formLayout.addRow(self.tr("日時"), self.show_day)
+        self.show_form_Layout.addRow(self.tr("公演名"), self.show_name)
+        self.show_form_Layout.addRow(self.tr("開場"), self.show_place)
+        self.show_form_Layout.addRow(self.tr("日時"), self.show_day)
 
-        self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.formLayout.setSpacing(5)  # レイアウトの余白を10に設定
-        self.setLayout(self.formLayout)  # 垂直レイアウトに水平レイアウトを追
+        self.crew_form_Layout = QFormLayout()
+        self.crew_stage_director = QLineEdit()
+        self.crew_sound_director = QLineEdit()
+        self.crew_sound_crew = QLineEdit()
+        self.crew_form_Layout.addRow(self.tr("舞台監督"), self.crew_stage_director)
+        self.crew_form_Layout.addRow(self.tr("音響監督"), self.crew_sound_director)
+        self.crew_form_Layout.addRow(self.tr("音響"), self.crew_sound_crew)
+
+        self.show_form_Layout.setContentsMargins(0, 0, 0, 0)
+        self.show_form_Layout.setSpacing(5)  # レイアウトの余白を5に設定
+        self.crew_form_Layout.setContentsMargins(0, 0, 0, 0)
+        self.crew_form_Layout.setSpacing(5)  # レイアウトの余白を5に設定
+        self.grid_layout.setContentsMargins(0, 0, 0, 0)
+        self.grid_layout.setSpacing(10)
+
+        self.grid_layout.addLayout(self.show_form_Layout, 0, 0)
+        self.grid_layout.addLayout(self.crew_form_Layout, 0, 1)
+        self.setLayout(self.grid_layout)
